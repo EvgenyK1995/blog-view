516fef3b11ea45dd30f68db1b2cef9e26e1288fa
EvgenyK1995 <you@example.com>
2019-02-19 18:45:13 +0500

    br-1 --setup and tuned vue-router

---
+++ blog-view/src/components/mainPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home-page">
+        <header class="home-page__header">
+            <h1>Hello</h1>
+        </header>
+    </div>
+</template>
+<script>
+export default {
+  name: 'mainPage'
+}
+</script>
+<style scoped>
+.home-page {
+    margin: 0 auto;
+    width: 900px;
+}
+.home-page__header {
+    color: #4b6b5f;
+    text-align: center;
+}
+</style>
---
+++ blog-view/src/router/routes.js
@@ -0,0 +1,5 @@
+import main from '../components/mainPage'
+
+export default [
+  { path: '/', name: 'main', component: main }
+]
